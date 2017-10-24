70a6a836dac171d95750c46cbacea4c3f9e8fad7
adam mcbride <adam.mcbride@gmail.com>
2017-10-23 20:11:39 -0400

    lesson 1

---
+++ coder-swag/View/CategoryCell.swift
@@ -0,0 +1,24 @@
+//
+//  CategoryCell.swift
+//  coder-swag
+//
+//  Created by Adam McBride on 10/23/17.
+//  Copyright © 2017 Adam McBride. All rights reserved.
+//
+
+import UIKit
+
+class CategoryCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
